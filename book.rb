3c3c32480aa030829f764eface9095450322c4ff
OpondoG <okonjogilbert2@gmail.com>
2023-01-07 18:59:41 +0300

    Modify book.rb file

--- book.rb
+++ book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
@@ -7,7 +7,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental(book)
-    @rentals.push(book)
+  def add_rental(person, date)
+    rental = Rental.new(date, self, person)
   end
 end
