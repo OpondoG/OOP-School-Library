c6f57dded7982b58bde7f9f69666ecc84fe5d526
OpondoG <okonjogilbert2@gmail.com>
2023-01-07 09:07:04 +0300

    Created the teacher & student and person classes

--- person.rb
+++ person.rb
@@ -1,27 +1,16 @@
-require_relative 'nameable'
-
-class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id
-
-  def initialize(name, age, parent_permission: true)
-    super()
+class Person
+  def initialize(age, name = 'Unknown', parent_permission = 'true')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_services?
-    if of_age? || @parent_permission
-      true
-    else
-      false
-    end
-  end
+  attr_accessor :name, :age
+  attr_reader :id
 
-  def correct_name
-    @name
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
