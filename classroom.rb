d1f824b24d95a2cbe6995b972706730c99e61201
OpondoG <okonjogilbert2@gmail.com>
2023-01-07 17:38:36 +0300

    Make changes

--- classroom.rb
+++ classroom.rb
@@ -8,23 +8,8 @@ class Classroom
     @students = []
   end
 
-  class Classroom
-    attr_accessor :label
-    attr_reader :students
-
-    def initialize(label)
-      @label = label
-      @students = []
-    end
-
     def add_student(student)
       @students.push(student) unless @students.include?(student)
       student.classroom = self
     end
-  end
-
-  def add_student(student)
-    @students.push(student) unless @students.include?(student)
-    student.classroom = self
-  end
 end
