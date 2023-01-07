37e68c48992548b11850d389ad36265a938f7dc8
OpondoG <okonjogilbert2@gmail.com>
2023-01-07 13:34:04 +0300

    Make changes required

--- person.rb
+++ person.rb
@@ -1,16 +1,32 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission = 'true')
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   attr_accessor :name, :age
   attr_reader :id
 
   def can_use_services?
-    of_age? || @parent_permission
+    if of_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rentals(person)
+    @rentals.push(person)
   end
 
   private
